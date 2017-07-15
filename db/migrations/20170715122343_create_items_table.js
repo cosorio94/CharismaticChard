00608eeee185637a804532385b3b78974e15f69f
Carlos Osorio <carlososoriov@gmail.com>
2017-07-15 14:31:51 -0700

    made item name in database not nullable

--- db/migrations/20170715122343_create_items_table.js
+++ db/migrations/20170715122343_create_items_table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('items', function (table) {
     table.increments('id').unsigned().primary();
-    table.string('item_name').nullable();
+    table.string('item_name').notNullable();
     table.decimal('price').notNullable();
     table.integer('payer_id').references('profiles.id');
     table.integer('debtor_id').references('profiles.id');
