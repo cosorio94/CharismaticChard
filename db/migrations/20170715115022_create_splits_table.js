cd8b944830cdd0657e7356c3234b771cc1464761
cosorio94 <carlososoriov@hotmail.com>
2017-07-15 14:48:43 -0700

    Feat/set database (#4)
    * add splits table to database
    
    * add splits table to database
    
    * made item name in database not nullable

---
+++ db/migrations/20170715115022_create_splits_table.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('splits', function (table) {
+    table.increments('id').unsigned().primary();
+    table.string('split_name').nullable();
+    table.decimal('total').notNullable();
+    table.decimal('tax').nullable();
+    table.decimal('tip').nullable();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('splits');
+};
---
+++ db/migrations/20170715122343_create_items_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('items', function (table) {
+    table.increments('id').unsigned().primary();
+    table.string('item_name').notNullable();
+    table.decimal('price').notNullable();
+    table.integer('payer_id').references('profiles.id');
+    table.integer('debtor_id').references('profiles.id');
+    table.integer('split_id').references('splits.id');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('items');
+};
