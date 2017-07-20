cd74477d3692a6f95001e2ed0993f6f93bae86ca
Carlos Osorio <carlososoriov@gmail.com>
2017-07-20 13:59:21 -0700

    add split name to twilio message

--- server/services/twilio/helper_twilio.js
+++ server/services/twilio/helper_twilio.js
@@ -2,7 +2,7 @@ module.exports.generateMessage = function (data) {
   let messages = []; 
   let owner = data.splitter.name;
   data.debtors.forEach( (debtor) => {
-    let message = `Hello ${debtor.name}, your total debt to ${owner} is ${debtor.debtTotal}. 
+    let message = `Hello ${debtor.name}, your total debt to ${owner} for ${data.splitName} is ${debtor.debtTotal}. 
                    items: ${debtor.items[0].itemName}-${debtor.items[0].itemPrice}, 
                    tax: ${debtor.tax} 
                    tip: ${debtor.tip}`;
