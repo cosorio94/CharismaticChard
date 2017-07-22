dee40b3290df5143b2f324a9a1fa8585a3e9d5b7
Carlos Osorio <carlososoriov@gmail.com>
2017-07-22 12:12:18 -0700

    get splitter data from database through user.id to save a split

--- server/controllers/splits.js
+++ server/controllers/splits.js
@@ -16,7 +16,8 @@ module.exports = {
 
   saveSplit: (req, res) => {
     // find with first name as well (or with user id)
-    return models.Profile.findOne({ phone: req.body.splitter.phone })
+    // return models.Profile.findOne({ phone: req.body.splitter.phone })
+    return models.Profile.findOne({ id: req.user.id })
       // .then(Controller.resolveErrors)
       .then(profile => {
         req.split['splitter_id'] = profile.get('id');
