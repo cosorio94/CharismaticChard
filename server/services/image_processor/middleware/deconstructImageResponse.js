f92cd1590fb462281189a1d2c5783f22d3e4c57b
Joseph Kwon <kwonjosepho@gmail.com>
2017-08-01 14:03:59 -0700

    (refactor) twilio n google cloud vision config refactor (#64)
    * (refactor) twilio and cloud vision config rework
    
    * (refactor) add CloudVision and Twilio key labels to example files

--- server/services/image_processor/middleware/deconstructImageResponse.js
+++ server/services/image_processor/middleware/deconstructImageResponse.js
@@ -2,8 +2,18 @@ const config = require('config')['cloudVision'];
 const helperFunctions = require('../helperFunctions');
 
 const vision = require('@google-cloud/vision')({
-  projectId: config.project_id,
-  credentials: config
+  projectId: process.env.CloudVision_project_id || config.project_id,
+  credentials: process.env.isHeroku ? {
+    type: process.env.CloudVision_type,
+    project_id: process.env.CloudVision_project_id,
+    private_key_id: process.env.CloudVision_private_key_id,
+    private_key: process.env.CloudVision_private_key,
+    client_id: process.env.CloudVision_client_id,
+    auth_uri: process.env.CloudVision_auth_uri,
+    token_uri: process.env.CloudVision_token_uri,
+    auth_provider_x509_cert_url: process.env.CloudVision_auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.CloudVision_client_x509_cert_url
+  } : config
 });
 
 var receipt1 = '/Users/carlososoriov/Documents/Hack\ Reactor/thesis/CharismaticChard/server/services/image_processor/test/receipt1.JPG';
--- server/services/twilio/twilio.js
+++ server/services/twilio/twilio.js
@@ -1,24 +1,13 @@
-if (process.env.Twilio_accountSid && process.env.Twilio_authToken) {
-  var config = {
-    accountSid: process.env.Twilio_accountSid,
-    authToken: process.env.Twilio_authToken
-  };
-} else if (!process.env.isTravis) {
-  var config = require('./config.js');
-}
-var twilio = require('twilio');
-if (process.env.isTravis) {
-  var client = {
-    messages: {
-      create: () => {
-        return;
-      }
-    }
-  };
-} else {
-  var client = new twilio(config.accountSid, config.authToken);
-}
-
+const twilio = require('twilio');
+const config = process.env.isHeroku ? {
+  accountSid: process.env.Twilio_accountSid,
+  authToken: process.env.Twilio_authToken
+} : require('config')['twilio'];
+const client = process.env.isTravis ? {
+  messages: {
+    create: () => null
+  }
+} : new twilio(config.accountSid, config.authToken);
 
 module.exports.sendSms = function(to, message) {
   return client.messages
