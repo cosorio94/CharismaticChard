161757b204c4417e7690c4ef0f16de3a1a3e2f1e
cosorio94 <carlososoriov@hotmail.com>
2017-07-31 18:23:13 -0700

    Feat/tax and total ocr (#60)
    * implement tax and total amount recognition from gcloud response
    
    * change tax and total property names in server request object

--- server/services/image_processor/middleware/filterImageResponse.js
+++ server/services/image_processor/middleware/filterImageResponse.js
@@ -4,5 +4,7 @@ module.exports = (req, res, next) => {
   req.itemLines = req.body.imageItems.map(itemBounds => {
     return helperFunctions.filterImageResponse(req.words, itemBounds, req.lines);
   });
+  req.totalLine = !!req.body.imageTotal ? helperFunctions.filterImageResponse(req.words, req.body.imageTotal, req.lines) : null;
+  req.taxLine = !!req.body.imageTax ? helperFunctions.filterImageResponse(req.words, req.body.imageTax, req.lines) : null;
   return next();
 };
