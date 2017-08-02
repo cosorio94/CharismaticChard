810d6f33610ea37c1266bac35064b2df66a59e42
Carlos Osorio <carlososoriov@gmail.com>
2017-08-02 12:09:50 -0700

    refactored item history page

--- client/src/components/historyItem.js
+++ client/src/components/historyItem.js
@@ -26,41 +26,40 @@ class SplitterHistoryItem extends React.Component {
   }
 
   render() {
-    return this.props.item ?
-      (
-        <div className="head">
-          <h3 className="homeWelcome">Item History</h3>
-          <div className="history-button">
-            <Link className="btn btn-primary" to="/history" >Split History</Link>
-            <Link className="btn btn-primary" to="/item">Item History</Link>
-          </div>
-          <div className="container-fluid">
-            { this.props.item.map((data, index) => (
-              <div className= "split-history" key={index}>
-                <div className="row">
-                  <label className="col-xs-6">Split Name: </label>
-                  <p className="col-xs-6">{data.split.split_name}</p>
-                </div>
-                <div className="row">
-                  <label className="col-xs-6">Splitter: </label>
-                  <p className="col-xs-6">{data.splitter.display}</p>
-                </div>
-                <div className="row">
-                  <label className="col-xs-6">Item: </label>
-                  <p className="col-xs-6">{data.item_name}</p>
-                </div>
-                <div className="row">
-                  <label className="col-xs-6">Price: </label>
-                  <p className="col-xs-6">{data.price}</p>
-                </div>
-              </div>
-            ))
-            }  
-          </div>
+    return this.props.item ? (
+      <div className="head">
+        <h3 className="homeWelcome">Item History</h3>
+        <div className="history-button">
+          <Link className="btn btn-primary" to="/history" >Split History</Link>
+          <Link className="btn btn-primary" to="/item">Item History</Link>
         </div>
-      ) : (
-        <Loading/>
-      );
+        <div className="container-fluid">
+          { this.props.item.map((data, index) => (
+            <div className= "split-history" key={index}>
+              <div className="row">
+                <label className="col-xs-6">Split Name: </label>
+                <p className="col-xs-6">{data.split.split_name}</p>
+              </div>
+              <div className="row">
+                <label className="col-xs-6">Splitter: </label>
+                <p className="col-xs-6">{data.splitter.display}</p>
+              </div>
+              <div className="row">
+                <label className="col-xs-6">Item: </label>
+                <p className="col-xs-6">{data.item_name}</p>
+              </div>
+              <div className="row">
+                <label className="col-xs-6">Price: </label>
+                <p className="col-xs-6">{data.price}</p>
+              </div>
+            </div>
+          ))
+          }  
+        </div>
+      </div>
+    ) : (
+      <Loading/>
+    );
   }
 }
 
